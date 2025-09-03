b89142d0196f663d657a490f0b94d25e9b1ae54f
Justin Miller <justamiller@msn.com>
2025-09-03 16:25:43 -0700

    fix bug

--- src/BPEngine.Tokenizer/Extensions.cs
+++ src/BPEngine.Tokenizer/Extensions.cs
@@ -8,12 +8,12 @@ namespace BPEngine.Tokenizer
 {
     public static class Extensions
     {
-        public static bool IsNullOrWhiteSpace(string? value)
+        public static bool IsNullOrWhiteSpace(this string? value)
         {
             return string.IsNullOrWhiteSpace(value);
         }
 
-        public static bool IsNullOrEmpty(string? value)
+        public static bool IsNullOrEmpty(this string? value)
         {
             return string.IsNullOrEmpty(value);
         }
